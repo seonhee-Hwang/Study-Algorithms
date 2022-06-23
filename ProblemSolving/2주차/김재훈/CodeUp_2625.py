5c658e89413ddbd580d34ace9c97be074f1ae601
Jackson <k9632441@gmail.com>
2022-06-23 23:10:09 +0900

    (김재훈) Problem Solving 2주차 코드 추가

--- ProblemSolving/2주차/김재훈/CodeUp_2625.py
+++ ProblemSolving/2주차/김재훈/CodeUp_2625.py
@@ -5,11 +5,12 @@ N = int(input())
 # N - 3, 1, 2
 
 result = set()
-for i in range(N-2, 1, -1):
+for i in range(N-2, 1, -1): # 7부터 2까지 동작
     for j in range(N - i - 1, 0, -1):
         tmp = [i, j, N - i - j]
         tmp.sort()
         if tmp[2] < tmp[0] + tmp[1]:
+            # print(''.join(map(str, tmp)))
             result.add(''.join(map(str, tmp)))
 
 print(len(result))
