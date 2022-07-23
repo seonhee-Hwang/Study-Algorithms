859749d8302d2c2cfd8bb98dcb838f3378656428
LittleForest9 <95530873+LittleForest9@users.noreply.github.com>
2022-07-24 07:55:42 +0900

    (구예빈) Problem Solving 3주차 코드 추가

---
+++ ProblemSolving/3주차/구예빈/BOJ_11399.py
@@ -0,0 +1,16 @@
+n = int(input())
+m = list(map(int, input().split()))
+m.sort()
+
+process_time = [0 for _ in range(n)]
+total_process_time = 0
+
+for i in range(n):
+    if i == 0:
+        process_time[i] = m[0]
+        total_process_time = m[0]
+    else:
+        process_time[i] = process_time[i-1] + m[i]
+        total_process_time += process_time[i]
+
+print(process_time)
